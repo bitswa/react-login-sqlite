49d5bd3e2971e70668c2235f383fe3b342f40db8
Gabriel Moura do Val <bitswa@pm.me>
2023-05-27 09:08:13 -0300

    error handling

--- frontend/src/error-page.tsx
+++ frontend/src/error-page.tsx
@@ -1,14 +1,17 @@
 import { useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
-	const error: unknown = useRouteError();
+	const error: any = useRouteError();
 
 	return (
 		<div className="flex flex-col gap-8 justify-center items-center h-screen text-center">
 			<h1 className="text-4xl font-bold">Oops!</h1>
 			<p>Sorry, an unexpected error has occurred.</p>
 			<p className="text-slate-400">
-				<i>{error.statusText || error.message}</i>
+				<i>
+					{(error as Error)?.message ||
+						(error as { statusText?: string })?.statusText}
+				</i>
 			</p>
 		</div>
 	);
