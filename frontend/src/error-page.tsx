68af4974729ff01cb1a55c22c266f01308bfb9e5
Gabriel Moura do Val <bitswa@pm.me>
2023-05-27 08:59:56 -0300

    erasing console.log

--- frontend/src/error-page.tsx
+++ frontend/src/error-page.tsx
@@ -1,16 +1,15 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
-  const error: unknown = useRouteError();
-  console.log(error);
+	const error: unknown = useRouteError();
 
-  return (
-    <div className="flex flex-col gap-8 justify-center items-center h-screen text-center">
-      <h1 className="text-4xl font-bold">Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p className="text-slate-400">
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-8 justify-center items-center h-screen text-center">
+			<h1 className="text-4xl font-bold">Oops!</h1>
+			<p>Sorry, an unexpected error has occurred.</p>
+			<p className="text-slate-400">
+				<i>{error.statusText || error.message}</i>
+			</p>
+		</div>
+	);
 }
