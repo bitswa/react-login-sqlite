d0e1ee99543ad4817c3eb5af7686e2f205be4986
Gabriel Moura do Val <bitswa@pm.me>
2023-05-27 09:12:02 -0300

    main formatted

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,33 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './routes/App.tsx'
-import Login from './routes/Login.tsx'
-import Register from "./routes/Register.tsx";
-import ErrorPage from "./error-page.tsx";
-import "./index.css";
+import App from './routes/App.tsx';
+import Login from './routes/Login.tsx';
+import Register from './routes/Register.tsx';
+import ErrorPage from './error-page.tsx';
+import './index.css';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-    errorElement: <ErrorPage />,
-  },
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: '/register',
+		element: <Register />,
+		errorElement: <ErrorPage />,
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
