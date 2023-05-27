00476e851786b028456c6793292e865172bf230e
Gabriel Moura do Val <bitswa@pm.me>
2023-05-27 09:09:07 -0300

    routes formatted

--- frontend/src/routes/App.tsx
+++ frontend/src/routes/App.tsx
@@ -1,168 +1,173 @@
-import { useEffect, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-import edit from "../assets/pen-to-square.svg";
+import edit from '../assets/pen-to-square.svg';
 
 interface User {
-  id: number;
-  name: string;
-  photo_url: string;
-  email: string;
-  created_at: string;
+	id: number;
+	name: string;
+	photo_url: string;
+	email: string;
+	created_at: string;
 }
 
 export default function App() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [user, setUser] = useState<User | null>(null);
-  const [enableNameInput, setEnableNameInput] = useState(true);
-  const [enablePhotoInput, setEnablePhotoInput] = useState(true);
+	const [user, setUser] = useState<User | null>(null);
+	const [enableNameInput, setEnableNameInput] = useState(true);
+	const [enablePhotoInput, setEnablePhotoInput] = useState(true);
 
-  const nameRef = useRef<HTMLInputElement>(null);
-  const photoRef = useRef<HTMLInputElement>(null);
-  const emailRef = useRef<HTMLInputElement>(null);
+	const nameRef = useRef<HTMLInputElement>(null);
+	const photoRef = useRef<HTMLInputElement>(null);
+	const emailRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
+	useEffect(() => {
+		const token = localStorage.getItem('token');
 
-    if (!token) {
-      navigate("/login");
-    }
+		if (!token) {
+			navigate('/login');
+		}
 
-    axios.get(`http://localhost:${import.meta.env.VITE_PORT}/`, {
-      headers: { Authorization: `Bearer ${token}` },
-    }).then(({ data }) => {
-      setUser(data);
-      if (nameRef.current !== null) nameRef.current.value = data?.name;
-      if (photoRef.current !== null) photoRef.current.value = data?.photo_url;
-      if (emailRef.current !== null) emailRef.current.value = data?.email;
-    }).catch((err) => {
-      console.log(err);
-      navigate("/login");
-    });
-  }, []);
+		axios
+			.get(`http://localhost:${import.meta.env.VITE_PORT}/`, {
+				headers: { Authorization: `Bearer ${token}` },
+			})
+			.then(({ data }) => {
+				setUser(data);
+				if (nameRef.current !== null) nameRef.current.value = data?.name;
+				if (photoRef.current !== null) photoRef.current.value = data?.photo_url;
+				if (emailRef.current !== null) emailRef.current.value = data?.email;
+			})
+			.catch((err) => {
+				console.log(err);
+				navigate('/login');
+			});
+	}, []);
 
-  const handleSignOut = () => {
-    localStorage.removeItem("token");
-    return navigate("/login");
-  };
+	const handleSignOut = () => {
+		localStorage.removeItem('token');
+		return navigate('/login');
+	};
 
-  const handleUpdate = () => {
-    setEnableNameInput(true);
-    setEnablePhotoInput(true);
-    axios.post(`http://localhost:${import.meta.env.VITE_PORT}/update`, {
-      id: user?.id,
-      name: nameRef?.current?.value,
-      photo: photoRef?.current?.value,
-    }).then(({ data }) => {
-      setUser(data);
-      if (nameRef.current !== null) nameRef.current.value = data?.name;
-      if (photoRef.current !== null) photoRef.current.value = data?.photo_url;
-      if (emailRef.current !== null) emailRef.current.value = data?.email;
-    })
-      .catch((err) => console.log(err));
-  };
+	const handleUpdate = () => {
+		setEnableNameInput(true);
+		setEnablePhotoInput(true);
+		axios
+			.post(`http://localhost:${import.meta.env.VITE_PORT}/update`, {
+				id: user?.id,
+				name: nameRef?.current?.value,
+				photo: photoRef?.current?.value,
+			})
+			.then(({ data }) => {
+				setUser(data);
+				if (nameRef.current !== null) nameRef.current.value = data?.name;
+				if (photoRef.current !== null) photoRef.current.value = data?.photo_url;
+				if (emailRef.current !== null) emailRef.current.value = data?.email;
+			})
+			.catch((err) => console.log(err));
+	};
 
-  return (
-    <div className="h-screen flex flex-col gap-8 justify-center items-center">
-      <div>
-        <img
-          className="w-16 h-16 rounded-full"
-          src={user?.photo_url}
-          alt="photo_url"
-        />
-      </div>
-      <div>
-        <ul className="flex flex-col gap-8">
-          <li className="flex flex-col gap-1 w-full">
-            <label
-              className="pl-3 text-sm md:text-[16px] font-medium"
-              htmlFor="name"
-            >
-              Name
-            </label>
-            <span className="h-[50px] rounded-xl text-gray-500 bg-[#627C85] bg-opacity-10 border border-[#627C85] flex justify-between overflow-hidden">
-              <input
-                className="outline-none p-2"
-                type="text"
-                name="name"
-                id="name"
-                ref={nameRef}
-                disabled={enableNameInput}
-              />
-              <button
-                onClick={() => setEnableNameInput((prev) => !prev)}
-                className="h-full grid place-items-center px-4"
-              >
-                <img className="w-4 h-4" src={edit} alt="edit" />
-              </button>
-            </span>
-          </li>
+	return (
+		<div className="h-screen flex flex-col gap-8 justify-center items-center">
+			<div>
+				<img
+					className="w-16 h-16 rounded-full"
+					src={user?.photo_url}
+					alt="photo_url"
+				/>
+			</div>
+			<div>
+				<ul className="flex flex-col gap-8">
+					<li className="flex flex-col gap-1 w-full">
+						<label
+							className="pl-3 text-sm md:text-[16px] font-medium"
+							htmlFor="name"
+						>
+							Name
+						</label>
+						<span className="h-[50px] rounded-xl text-gray-500 bg-[#627C85] bg-opacity-10 border border-[#627C85] flex justify-between overflow-hidden">
+							<input
+								className="outline-none p-2"
+								type="text"
+								name="name"
+								id="name"
+								ref={nameRef}
+								disabled={enableNameInput}
+							/>
+							<button
+								onClick={() => setEnableNameInput((prev) => !prev)}
+								className="h-full grid place-items-center px-4"
+							>
+								<img className="w-4 h-4" src={edit} alt="edit" />
+							</button>
+						</span>
+					</li>
 
-          <li className="flex flex-col gap-1 w-full">
-            <label
-              className="pl-3 text-sm md:text-[16px] font-medium"
-              htmlFor="photo"
-            >
-              Photo URL
-            </label>
-            <span className="h-[50px] rounded-xl text-gray-500 bg-[#627C85] bg-opacity-10 border border-[#627C85] flex justify-between overflow-hidden">
-              <input
-                className="outline-none p-2"
-                type="text"
-                name="photo"
-                id="photo"
-                ref={photoRef}
-                disabled={enablePhotoInput}
-              />
-              <button
-                onClick={() => setEnablePhotoInput((prev) => !prev)}
-                className="h-full grid place-items-center px-4"
-              >
-                <img className="w-4 h-4" src={edit} alt="edit" />
-              </button>
-            </span>
-          </li>
+					<li className="flex flex-col gap-1 w-full">
+						<label
+							className="pl-3 text-sm md:text-[16px] font-medium"
+							htmlFor="photo"
+						>
+							Photo URL
+						</label>
+						<span className="h-[50px] rounded-xl text-gray-500 bg-[#627C85] bg-opacity-10 border border-[#627C85] flex justify-between overflow-hidden">
+							<input
+								className="outline-none p-2"
+								type="text"
+								name="photo"
+								id="photo"
+								ref={photoRef}
+								disabled={enablePhotoInput}
+							/>
+							<button
+								onClick={() => setEnablePhotoInput((prev) => !prev)}
+								className="h-full grid place-items-center px-4"
+							>
+								<img className="w-4 h-4" src={edit} alt="edit" />
+							</button>
+						</span>
+					</li>
 
-          <li className="flex flex-col">
-            <label
-              className="pl-3 text-sm md:text-[16px] font-medium"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="h-[50px] rounded-xl text-gray-500 outline-none bg-[#627C85] bg-opacity-10 border border-[#627C85] p-2"
-              type="email"
-              name="email"
-              id="email"
-              ref={emailRef}
-              disabled
-            />
-          </li>
-          <li className="flex flex-col text-center">
-            <label className="pl-3 text-sm md:text-[16px] font-medium">
-              Criado em
-            </label>
-            <span>{user?.created_at}</span>
-          </li>
-        </ul>
-      </div>
-      <div className="flex gap-6">
-        <button
-          className="bg-[#000] text-[#FFF] px-4 py-2"
-          onClick={handleSignOut}
-        >
-          Sair
-        </button>
-        <button
-          className="bg-[#000] text-[#FFF] px-4 py-2"
-          onClick={handleUpdate}
-        >
-          Atualizar
-        </button>
-      </div>
-    </div>
-  );
+					<li className="flex flex-col">
+						<label
+							className="pl-3 text-sm md:text-[16px] font-medium"
+							htmlFor="email"
+						>
+							Email
+						</label>
+						<input
+							className="h-[50px] rounded-xl text-gray-500 outline-none bg-[#627C85] bg-opacity-10 border border-[#627C85] p-2"
+							type="email"
+							name="email"
+							id="email"
+							ref={emailRef}
+							disabled
+						/>
+					</li>
+					<li className="flex flex-col text-center">
+						<label className="pl-3 text-sm md:text-[16px] font-medium">
+							Criado em
+						</label>
+						<span>{user?.created_at}</span>
+					</li>
+				</ul>
+			</div>
+			<div className="flex gap-6">
+				<button
+					className="bg-[#000] text-[#FFF] px-4 py-2"
+					onClick={handleSignOut}
+				>
+					Sair
+				</button>
+				<button
+					className="bg-[#000] text-[#FFF] px-4 py-2"
+					onClick={handleUpdate}
+				>
+					Atualizar
+				</button>
+			</div>
+		</div>
+	);
 }
--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,140 +1,143 @@
-import { FormEvent, useEffect, useRef } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { FormEvent, useEffect, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-import arrow from "../assets/arrow-right.svg";
-import google from "../assets/google.svg";
-import facebook from "../assets/facebook.svg";
-import illustration from "../assets/illustration.svg";
+import arrow from '../assets/arrow-right.svg';
+import google from '../assets/google.svg';
+import facebook from '../assets/facebook.svg';
+import illustration from '../assets/illustration.svg';
 
 function Login() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
+	const emailRef = useRef<HTMLInputElement>(null);
+	const passwordRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
+	useEffect(() => {
+		const token = localStorage.getItem('token');
 
-    if (token) {
-      axios.get(`http://localhost:${import.meta.env.VITE_PORT}/`, {
-        headers: { Authorization: `Bearer ${token}` },
-      }).then(() => {
-        navigate("/");
-      }).catch((err) => {
-        console.log(err);
-      });
-    }
-  }, []);
+		if (token) {
+			axios
+				.get(`http://localhost:${import.meta.env.VITE_PORT}/`, {
+					headers: { Authorization: `Bearer ${token}` },
+				})
+				.then(() => {
+					navigate('/');
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	}, []);
 
-  const handleLoginSubmit = (event: FormEvent) => {
-    event.preventDefault();
+	const handleLoginSubmit = (event: FormEvent) => {
+		event.preventDefault();
 
-    const email = (emailRef?.current?.value)?.toLowerCase();
+		const email = emailRef?.current?.value?.toLowerCase();
 
-    const password = passwordRef?.current?.value;
-    if (password && password?.length < 6) return;
+		const password = passwordRef?.current?.value;
+		if (password && password?.length < 6) return;
 
-    axios
-      .post(`http://localhost:${import.meta.env.VITE_PORT}/login`, {
-        email,
-        password,
-      })
-      .then(({ data }) => {
-        localStorage.setItem("token", data.token);
-        return navigate("/");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+		axios
+			.post(`http://localhost:${import.meta.env.VITE_PORT}/login`, {
+				email,
+				password,
+			})
+			.then(({ data }) => {
+				localStorage.setItem('token', data.token);
+				return navigate('/');
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-  return (
-    <div className="flex items-center justify-center lg:justify-around p-6 h-screen bg-[#FFF]">
-      <div className="w-full max-w-xl lg:w-[576px] flex flex-col h-full justify-around">
-        <div className="flex justify-between lg:flex-col gap-6">
-          <h1 className="font-bold text-3xl md:text-4xl">Sign in</h1>
-          <div className="hidden lg:flex items-center gap-6">
-            <span className="border-2 border-[#000] h-[1px] w-[80px]" />
-            <span className="font-bold text-[16px]">Login with</span>
-          </div>
-          <div className="flex gap-6">
-            <button className="flex items-center justify-around lg:py-2 lg:px-4 lg:h-[50px] lg:w-full lg:border border-[#627C85] rounded-xl">
-              <img src={google} alt="Google" />
-              <span className="hidden lg:block text-[16px]">
-                Login with Google
-              </span>
-            </button>
-            <button className="flex items-center justify-around lg:py-2 lg:px-4 lg:h-[50px] lg:w-full lg:border border-[#627C85] rounded-xl">
-              <img src={facebook} alt="Facebook" />
-              <span className="hidden lg:block text-[16px]">
-                Login with Facebook
-              </span>
-            </button>
-          </div>
-        </div>
-        <form onSubmit={handleLoginSubmit} method="post">
-          <div className="flex flex-col gap-6">
-            <span className="flex flex-col gap-1 w-full">
-              <label
-                className="pl-3 text-sm md:text-[16px] font-medium"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <input
-                className="h-[50px] rounded-xl text-gray-500 outline-none bg-[#627C85] bg-opacity-10 border border-[#627C85] p-2"
-                type="email"
-                name="email"
-                id="email"
-                required
-                ref={emailRef}
-              />
-            </span>
+	return (
+		<div className="flex items-center justify-center lg:justify-around p-6 h-screen bg-[#FFF]">
+			<div className="w-full max-w-xl lg:w-[576px] flex flex-col h-full justify-around">
+				<div className="flex justify-between lg:flex-col gap-6">
+					<h1 className="font-bold text-3xl md:text-4xl">Sign in</h1>
+					<div className="hidden lg:flex items-center gap-6">
+						<span className="border-2 border-[#000] h-[1px] w-[80px]" />
+						<span className="font-bold text-[16px]">Login with</span>
+					</div>
+					<div className="flex gap-6">
+						<button className="flex items-center justify-around lg:py-2 lg:px-4 lg:h-[50px] lg:w-full lg:border border-[#627C85] rounded-xl">
+							<img src={google} alt="Google" />
+							<span className="hidden lg:block text-[16px]">
+								Login with Google
+							</span>
+						</button>
+						<button className="flex items-center justify-around lg:py-2 lg:px-4 lg:h-[50px] lg:w-full lg:border border-[#627C85] rounded-xl">
+							<img src={facebook} alt="Facebook" />
+							<span className="hidden lg:block text-[16px]">
+								Login with Facebook
+							</span>
+						</button>
+					</div>
+				</div>
+				<form onSubmit={handleLoginSubmit} method="post">
+					<div className="flex flex-col gap-6">
+						<span className="flex flex-col gap-1 w-full">
+							<label
+								className="pl-3 text-sm md:text-[16px] font-medium"
+								htmlFor="email"
+							>
+								Email
+							</label>
+							<input
+								className="h-[50px] rounded-xl text-gray-500 outline-none bg-[#627C85] bg-opacity-10 border border-[#627C85] p-2"
+								type="email"
+								name="email"
+								id="email"
+								required
+								ref={emailRef}
+							/>
+						</span>
 
-            <span className="flex flex-col gap-1 max-w-full">
-              <label
-                className="pl-3 text-sm md:text-[16px] font-medium"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <input
-                className="h-[50px] rounded-xl text-gray-500 outline-none bg-[#627C85] bg-opacity-10 border border-[#627C85] p-2"
-                type="password"
-                name="password"
-                id="password"
-                required
-                minLength={6}
-                ref={passwordRef}
-              />
-            </span>
-          </div>
+						<span className="flex flex-col gap-1 max-w-full">
+							<label
+								className="pl-3 text-sm md:text-[16px] font-medium"
+								htmlFor="password"
+							>
+								Password
+							</label>
+							<input
+								className="h-[50px] rounded-xl text-gray-500 outline-none bg-[#627C85] bg-opacity-10 border border-[#627C85] p-2"
+								type="password"
+								name="password"
+								id="password"
+								required
+								minLength={6}
+								ref={passwordRef}
+							/>
+						</span>
+					</div>
 
-          <button
-            className="flex justify-center items-center mt-6 bg-[#F34848] hover:bg-opacity-90 active:bg-opacity-90 rounded-3xl w-[60px] h-[60px] md:w-[70px] md:h-[70px] shadow-xl"
-            type="submit"
-          >
-            <img src={arrow} alt="send" />
-          </button>
-        </form>
+					<button
+						className="flex justify-center items-center mt-6 bg-[#F34848] hover:bg-opacity-90 active:bg-opacity-90 rounded-3xl w-[60px] h-[60px] md:w-[70px] md:h-[70px] shadow-xl"
+						type="submit"
+					>
+						<img src={arrow} alt="send" />
+					</button>
+				</form>
 
-        <span className="text-sm md:text-[16px]">
-          Don't have an account yet?{" "}
-          <Link to="/register" className="text-[#627C85] underline">
-            Sign up
-          </Link>
-        </span>
-      </div>
-      <div className="my-auto">
-        <img
-          src={illustration}
-          alt="illustration"
-          className="hidden w-[500px] lg:block"
-        />
-      </div>
-    </div>
-  );
+				<span className="text-sm md:text-[16px]">
+					Don't have an account yet?{' '}
+					<Link to="/register" className="text-[#627C85] underline">
+						Sign up
+					</Link>
+				</span>
+			</div>
+			<div className="my-auto">
+				<img
+					src={illustration}
+					alt="illustration"
+					className="hidden w-[500px] lg:block"
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
